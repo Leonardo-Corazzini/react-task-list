df047ff5746be91717ef494fbe559b28341f3528
Leonardo-Corazzini <leocora10@gmail.com>
2024-11-26 16:27:46 +0100

    ADD: style

--- react-task-list/src/App.jsx
+++ react-task-list/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import tasks from './tasks'
 
 function App() {
-
+  const title = 'Task Manager'
   const taskInProgress = tasks.filter((task) => (task.state !== 'completed'))
   console.log(taskInProgress)
   const taskCompleted = tasks.filter((task) => (task.state === 'completed'))
@@ -10,19 +10,42 @@ function App() {
 
 
   return (
-    <main>
-      <ul>
-        {taskInProgress.map((task) =>
-          <li key={task.id}>{task.title}</li>
-        )}
-      </ul>
-      <ul>
-        {taskCompleted.map((task) =>
-          <li key={task.id}>{task.title}</li>
-        )}
-      </ul>
+    <>
+      <header className='header'>
+        <h1>{title}</h1>
+      </header>
+      <main>
+        <div className='task-inprogress'>
+          <ul>
+            <h2>Current Task ({taskInProgress.length})</h2>
+            {taskInProgress.map((task) =>
+              <li key={task.id}>
+                <p>{task.title} <span className='tag'>{task.state}</span></p>
+                <p>Priority: {task.priority}</p>
+                <p>Est. time {task.estimatedTime}</p>
 
-    </main>
+              </li>
+            )}
+          </ul>
+
+        </div>
+        <div className='task-completed'>
+          <ul>
+            <h2>Current Task ({taskCompleted.length})</h2>
+            {taskCompleted.map((task) =>
+              <li key={task.id}>
+                <p>{task.title} <span className='tag'>{task.state}</span></p>
+                <p>Priority: {task.priority}</p>
+                <p>Est. time {task.estimatedTime}</p>
+              </li>
+            )}
+          </ul>
+        </div>
+
+      </main>
+
+    </>
+
   )
 
 
